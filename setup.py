67134c0cde2ebb2141ee68881966d0ef3b5efb48
dongweiming <ciici1234@hotmail.com>
2015-01-11 22:02:38 +0800

    Use a better way find package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
@@ -20,7 +20,7 @@ setup(
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
 
-    packages=['aspy.yaml'],
+    packages=find_packages(exclude=['tests*']),
     namespace_packages=['aspy'],
     install_requires=[
         'ordereddict',
