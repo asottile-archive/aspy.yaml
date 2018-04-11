01c8decae3dfd20d780296daa8d2e117628faddf
Anthony Sottile <asottile@umich.edu>
2018-04-11 09:02:10 -0700

    v1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='aspy.yaml',
     description="A few extensions to pyyaml.",
     url='https://github.comasottile/aspy.yaml',
-    version='1.0.0',
+    version='1.1.0',
 
     author='Anthony Sottile',
     author_email='asottile@umich.edu',
@@ -14,11 +14,10 @@ setup(
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
 
