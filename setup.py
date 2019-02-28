cd73a673333fde5a3b295a098e7f5fa071a0ed08
Anthony Sottile <asottile@umich.edu>
2019-02-27 22:08:45 -0800

    Migrate setup.py to setup.cfg declarative metadata
    Committed via https://github.com/asottile/all-repos

--- setup.py
+++ setup.py
@@ -1,27 +1,2 @@
-from setuptools import find_packages
 from setuptools import setup
-
-
-setup(
-    name='aspy.yaml',
-    description="A few extensions to pyyaml.",
-    url='https://github.com/asottile/aspy.yaml',
-    version='1.1.2',
-
-    author='Anthony Sottile',
-    author_email='asottile@umich.edu',
-
-    platforms='all',
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ],
-
-    packages=find_packages(exclude=('tests*', 'testing*')),
-    install_requires=['pyyaml'],
-)
+setup()
